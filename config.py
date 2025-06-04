21b9709ba944e207ead504bbef44c5ffd3cc57fb
Gxinfinity <190551265+Gxinfinity@users.noreply.github.com>
2025-06-04 16:24:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,76 +1,47 @@
 import re
-from os import getenv
-
+import os
 from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
 
-# Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID", "29870111"))
-API_HASH = getenv("60848922c00cd07114ea65dae9ec9e13")
+# Required credentials
+API_ID = 29870111
+API_HASH = "60848922c00cd07114ea65dae9ec9e13"
+BOT_TOKEN = "7527646500:AAHkuB3sTA6GAdwMvQD31AB-5rbzPY6gnyE"
+MONGO_DB_URI = "mongodb+srv://mikey:mikey@cluster0.zv6knh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+OWNER_ID = 7290350162
+LOG_GROUP_ID = -1002621935143
 
-# Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("7527646500:AAHkuB3sTA6GAdwMvQD31AB-5rbzPY6gnyE")
+# Optional / deployment-related
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-# Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://mikey:mikey@cluster0.zv6knh9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/SpyGamer7t2r/AviaxMusic")
+UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "master")
+GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 10000))
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/gxinfinity_support")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/infinitygx_bot_support")
+PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
 
-# Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002621935143"))
+DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", 10000))
+DURATION_LIMIT = DURATION_LIMIT_MIN * 60
 
-# Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID","7290350162"))
+AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 
-## Fill these variables if you're deploying on heroku.
-# Your heroku app name
-HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-# Get it from http://dashboard.heroku.com/account
-HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", None)
+PLAYLIST_FETCH_LIMIT = int(os.getenv("PLAYLIST_FETCH_LIMIT", 25))
 
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/SpyGamer7t2r/AviaxMusic",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # Fill this variable if your upstream repository is private
+TG_AUDIO_FILESIZE_LIMIT = int(os.getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(os.getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/gxinfinity_support")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/infinitygx_bot_support")
-
-# Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-
-# make your bots privacy from telegra.ph and put your url here 
-PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-AviaxMusic-08-14")
-
-
-# Get this credentials from https://developer.spotify.com/dashboard
-SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
-SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
-
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
-
-
-# Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
-# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
-
-
-# Get your pyrogram v2 session from Replit
-STRING1 = getenv("STRING_SESSION", "BQHHyB8AWxKyNJmGhHwlJivp61nGpX_FGHO96G5CHXSn8HPzkcbCm78bwX9QCKCsBjPhdnAUGjt2vbdcIVvzOd_5xiVWUbzr-X1hA__AHps8iI_1ZhyXx2ryvCI1xCk59fOKkFq0JjE93Q6QzZBTfGS2nfUiiMc9McHQ0WOociwsrfgxJkaQWk4a7dr4RUYVaI14eon5MloA7X0zaVK0uRCuPSEXoGC0OmcI2H465aIc8ULE74oP9iKTAZsu9Ezat03Dn5P4szR-qFv2Sv6A-Ysi2Ccmuir3r_mDc81OEvKRhLQucr44wwNGZsPFSwO6YSW8RxAbeRjfYqhCJIPLa8FsoNANgAAAAAGiWzDAAA")
-STRING2 = getenv("STRING_SESSION2", None)
-STRING3 = getenv("STRING_SESSION3", None)
-STRING4 = getenv("STRING_SESSION4", None)
-STRING5 = getenv("STRING_SESSION5", None)
-
+STRING1 = "BQHHyB8AWxKyNJmGhHwlJivp61nGpX_FGHO96G5CHXSn8HPzkcbCm78bwX9QCKCsBjPhdnAUGjt2vbdcIVvzOd_5xiVWUbzr-X1hA__AHps8iI_1ZhyXx2ryvCI1xCk59fOKkFq0JjE93Q6QzZBTfGS2nfUiiMc9McHQ0WOociwsrfgxJkaQWk4a7dr4RUYVaI14eon5MloA7X0zaVK0uRCuPSEXoGC0OmcI2H465aIc8ULE74oP9iKTAZsu9Ezat03Dn5P4szR-qFv2Sv6A-Ysi2Ccmuir3r_mDc81OEvKRhLQucr44wwNGZsPFSwO6YSW8RxAbeRjfYqhCJIPLa8FsoNANgAAAAAGiWzDAAA"
+STRING2 = os.getenv("STRING_SESSION2", None)
+STRING3 = os.getenv("STRING_SESSION3", None)
+STRING4 = os.getenv("STRING_SESSION4", None)
+STRING5 = os.getenv("STRING_SESSION5", None)
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -79,13 +50,9 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
-
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg"
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
-)
+# Image URLs
+START_IMG_URL = os.getenv("START_IMG_URL", "https://graph.org//file/25115719697ed91ef5672.jpg")
+PING_IMG_URL = os.getenv("PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png")
 PLAYLIST_IMG_URL = "https://graph.org//file/3dfcffd0c218ead96b102.png"
 STATS_IMG_URL = "https://graph.org//file/99a8a9c13bb01f9ac7d98.png"
 TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
@@ -97,23 +64,14 @@ SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
-
+# Time converter
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
+# Validate links
+if SUPPORT_CHANNEL and not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+    raise SystemExit("[ERROR] - Your SUPPORT_CHANNEL url is wrong. It must start with https://")
 
-DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
-
-
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
-
-if SUPPORT_GROUP:
-    if not re.match("(?:http|https)://", SUPPORT_GROUP):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
-        )
+if SUPPORT_GROUP and not re.match("(?:http|https)://", SUPPORT_GROUP):
+    raise SystemExit("[ERROR] - Your SUPPORT_GROUP url is wrong. It must start with https://")
